0e8878343c740077628619c383ca9013c6adf31c
ofsouzap <ofsouzap@gmail.com>
2024-04-05 20:27:14 +0100

    Right hand improvements

--- Assets/Scripts/Characters/Hand.cs
+++ Assets/Scripts/Characters/Hand.cs
@@ -6,9 +6,22 @@ public class Hand : MonoBehaviour
     [SerializeField] private Animator _animator;
     protected Animator Animator => _animator;
 
+    private const string AnimParamThumbTouched = "ThumbTouched";
+    private const string AnimParamTriggerTouched = "TriggerTouched";
+    private const string AnimParamGripTouched = "GripTouched";
     private const string AnimParamThumbVal = "ThumbVal";
     private const string AnimParamTriggerVal = "TriggerVal";
     private const string AnimParamGripVal = "GripVal";
+
+    [SerializeField] private InputActionReference _thumbTouchedAction;
+    protected InputAction ThumbTouchedAction => _thumbTouchedAction.action;
+
+    [SerializeField] private InputActionReference _triggerTouchedAction;
+    protected InputAction TriggerTouchedAction => _triggerTouchedAction.action;
+
+    [SerializeField] private InputActionReference _gripTouchedAction;
+    protected InputAction GripTouchedAction => _gripTouchedAction.action;
+
 
     [SerializeField] private InputActionReference _thumbValAction;
     protected InputAction ThumbValAction => _thumbValAction.action;
@@ -29,6 +42,9 @@ public class Hand : MonoBehaviour
 
     protected virtual void Update()
     {
+        Animator.SetBool(AnimParamThumbTouched, ThumbTouchedAction.ReadValue<float>() > .5f);
+        Animator.SetBool(AnimParamTriggerTouched, TriggerTouchedAction.ReadValue<float>() > .5f);
+        Animator.SetBool(AnimParamGripTouched, GripTouchedAction.ReadValue<float>() > .5f);
         Animator.SetFloat(AnimParamThumbVal, ThumbValAction.ReadValue<float>());
         Animator.SetFloat(AnimParamTriggerVal, TriggerValAction.ReadValue<float>());
         Animator.SetFloat(AnimParamGripVal, GripValAction.ReadValue<float>());
