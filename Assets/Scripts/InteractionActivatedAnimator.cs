485c3e3501e61c9e61e6f558fd37ac722982ef25
ofsouzap <ofsouzap@gmail.com>
2024-04-07 21:29:06 +0100

    Use new environment models in scene

---
+++ Assets/Scripts/InteractionActivatedAnimator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace BonesVr
+{
+    public class InteractionActivatedAnimator : MonoBehaviour
+    {
+        [Tooltip("The animator to affect")]
+        [SerializeField] private Animator _animator;
+        protected Animator Animator => _animator;
+
+        [SerializeField] private string _boolParamName;
+        protected string BoolParamName => _boolParamName;
+
+        protected virtual void Awake()
+        {
+            if (Animator == null)
+                _animator = GetComponentInChildren<Animator>();
+            if (Animator == null)
+                Debug.LogWarning("No animator set or found");
+        }
+
+        public void EnableToggle()
+        {
+            Animator.SetBool(BoolParamName, true);
+        }
+
+        public void DisableToggle()
+        {
+            Animator.SetBool(BoolParamName, false);
+        }
+    }
+}
