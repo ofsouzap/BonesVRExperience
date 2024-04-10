a7a07210cd9870b917d3718ccff469d75914c142
ofsouzap <ofsouzap@gmail.com>
2024-04-10 21:36:21 +0100

    Fix bone arrangement minigame gizmo transforms

--- Assets/Scripts/Minigames/Arrangement/BoneArrangementSocketController.cs
+++ Assets/Scripts/Minigames/Arrangement/BoneArrangementSocketController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using BonesVr.Utils;
 
 namespace BonesVr.Minigames.Arrangement
 {
@@ -8,28 +9,116 @@ namespace BonesVr.Minigames.Arrangement
     {
         public XRSocketInteractor Interactor => GetComponent<XRSocketInteractor>();
 
-        protected Vector3 AttachPosition => Interactor.attachTransform != null ? Interactor.attachTransform.position : transform.position;
-        protected Quaternion AttachRotation => Interactor.attachTransform != null ? Interactor.attachTransform.rotation : transform.rotation;
-        protected Vector3 AttachScale => Interactor.attachTransform != null ? Interactor.attachTransform.localScale : transform.localScale;
+        private TRS InteractorAttachTrs => TRS.FromTransform(Interactor.attachTransform.transform);
+        private TRS InteractorTransformTrs => TRS.FromTransform(Interactor.transform);
 
-        [Header("Gizmo")]
+        protected TRS InteractorTrs => Interactor.attachTransform != null ? InteractorAttachTrs : InteractorTransformTrs;
+        protected Matrix4x4 InteractorTrsMat => TransformMatrices.FromTrs(InteractorTrs);
 
-        [SerializeField] private Mesh _gizmoMesh;
-        protected Mesh GizmoMesh => _gizmoMesh;
+        //protected Vector3 InteractorAttachPosition => Interactor.attachTransform != null ? Interactor.attachTransform.position : transform.position;
+        //protected Quaternion InteractorAttachRotation => Interactor.attachTransform != null ? Interactor.attachTransform.rotation : transform.rotation;
+        //protected Vector3 InteractorAttachScale => Interactor.attachTransform != null ? Interactor.attachTransform.localScale : transform.localScale;
 
-        [SerializeField] private Quaternion _gizmoRotation = Quaternion.identity;
-        protected Quaternion GizmoRotation => _gizmoRotation;
+        [Header("Preview Gizmo")]
 
-        [SerializeField] private Vector3 _gizmoScale = Vector3.one;
-        protected Vector3 GizmoScale => _gizmoScale;
+        [SerializeField] private Mesh _previewGizmoMesh;
+        protected Mesh PreviewGizmoMesh => _previewGizmoMesh;
+
+        [Tooltip("The game object (this can be a prefab) with an XR grab interactable component for the preview. This is used to find an attach transform for the preview gizmo")]
+        [SerializeField] private GameObject _previewInteractableGameObject;
+
+        [SerializeField] private Vector3 _previewGizmoPosition = Vector3.zero;
+        [SerializeField] private Quaternion _previewGizmoRotation = Quaternion.identity;
+        [SerializeField] private Vector3 _previewGizmoScale = Vector3.one;
+
+        protected Matrix4x4 GetGizmoExtraTrsMat()
+            => TransformMatrices.FromTrs(_previewGizmoPosition, _previewGizmoRotation, _previewGizmoScale);
+
+        protected XRGrabInteractable GetPreviewInteractable()
+        {
+            if (_previewInteractableGameObject == null)
+                return null;
+            else
+                return _previewInteractableGameObject.GetComponent<XRGrabInteractable>();
+        }
+
+        protected Matrix4x4 GetPreviewInteractableAttachTrsMat()
+        {
+            XRGrabInteractable interactable = GetPreviewInteractable();
+
+            if (interactable == null || interactable.attachTransform == null)
+                return Matrix4x4.identity;
+            else
+            {
+                Matrix4x4 attachMat = TransformMatrices.FromTransform(interactable.attachTransform.transform);
+                Matrix4x4 interactableMat = TransformMatrices.FromTransform(interactable.transform);
+                return interactableMat * attachMat.inverse;
+            }
+        }
+
+        //protected Vector3 GetInteractableAttachPosition()
+        //{
+        //    XRGrabInteractable interactable = GetPreviewInteractable();
+
+        //    if (interactable == null || interactable.attachTransform == null)
+        //        return Vector3.zero;
+        //    else
+        //        return interactable.transform.position - interactable.attachTransform.position;
+
+        //}
+
+        //protected Quaternion GetInteractableAttachRotation()
+        //{
+        //    XRGrabInteractable interactable = GetPreviewInteractable();
+
+        //    if (interactable == null || interactable.attachTransform == null)
+        //        return Quaternion.identity;
+        //    else
+        //        return interactable.transform.rotation * Quaternion.Inverse(interactable.attachTransform.rotation);
+        //}
+
+        //protected Vector3 GetInteractableAttachScale()
+        //{
+        //    XRGrabInteractable interactable = GetPreviewInteractable();
+
+        //    if (interactable == null || interactable.attachTransform == null)
+        //        return Vector3.one;
+        //    else
+        //        return new(
+        //            interactable.transform.lossyScale.x / interactable.attachTransform.lossyScale.x,
+        //            interactable.transform.lossyScale.y / interactable.attachTransform.lossyScale.y,
+        //            interactable.transform.lossyScale.z / interactable.attachTransform.lossyScale.z
+        //        );
+        //}
+
+        //protected Vector3 GetGizmoPosition() => InteractorAttachPosition + GetInteractableAttachPosition() + _previewGizmoPosition;
+        //protected Quaternion GetGizmoRotation() => InteractorAttachRotation * GetInteractableAttachRotation() * _previewGizmoRotation;
+        //protected Vector3 GetGizmoScale()
+        //{
+        //    Vector3 scale = _previewGizmoScale;
+        //    scale.Scale(InteractorAttachScale);
+        //    scale.Scale(GetInteractableAttachScale());
+        //    return scale;
+        //}
+
+        protected Matrix4x4 GetPreviewGizmoTrsMat()
+            => InteractorTrsMat * GetPreviewInteractableAttachTrsMat() * GetGizmoExtraTrsMat();
+
+        protected TRS GetPreviewGizmoTrs()
+            => TransformMatrices.ToTrs(GetPreviewGizmoTrsMat());
+
+        protected virtual void OnValidate()
+        {
+            if (_previewInteractableGameObject != null && GetPreviewInteractable() == null)
+                Debug.LogError("Preview interactable game object has no XR grab interactable component");
+        }
 
         protected virtual void OnDrawGizmos()
         {
-            if (GizmoMesh != null)
+            if (PreviewGizmoMesh != null)
             {
-                Vector3 scale = AttachScale;
-                scale.Scale(GizmoScale);
-                Gizmos.DrawWireMesh(GizmoMesh, AttachPosition, AttachRotation * GizmoRotation, scale);
+                TRS trs = GetPreviewGizmoTrs();
+                Gizmos.DrawWireMesh(PreviewGizmoMesh, trs.position, trs.rotation, trs.scale);
             }
         }
     }
---
+++ Assets/Scripts/Utils/TransformMatrices.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace BonesVr.Utils
+{
+    public readonly struct TRS
+    {
+        public readonly Vector3 position;
+        public readonly Quaternion rotation;
+        public readonly Vector3 scale;
+
+        public TRS(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.scale = scale;
+        }
+
+        public static TRS FromTransform(Transform transform)
+            => new(transform.position, transform.rotation, transform.lossyScale);
+
+        public static TRS Identity => new(Vector3.zero, Quaternion.identity, Vector3.one);
+    }
+
+    public static class TransformMatrices
+    {
+        public static Matrix4x4 FromTrs(Vector3 position, Quaternion rotation, Vector3 scale)
+            => Matrix4x4.TRS(position, rotation, scale);
+
+        public static Matrix4x4 FromTrs(TRS trs)
+            => FromTrs(trs.position, trs.rotation, trs.scale);
+
+        public static Matrix4x4 FromTransform(Transform transform)
+            => FromTrs(transform.position, transform.rotation, transform.lossyScale);
+
+        public static TRS ToTrs(Matrix4x4 mat)
+        {
+            // See https://discussions.unity.com/t/how-to-decompose-a-trs-matrix/63681
+
+            Vector3 position;
+            Quaternion rotation;
+            Vector3 scale;
+
+            position = mat.GetColumn(3);
+            rotation = Quaternion.LookRotation(mat.GetColumn(2), mat.GetColumn(1));
+            scale = new(
+                mat.GetColumn(0).magnitude,
+                mat.GetColumn(1).magnitude,
+                mat.GetColumn(2).magnitude
+            );
+
+            return new(position, rotation, scale);
+        }
+
+        public static void ToTrs(Matrix4x4 mat, out Vector3 position, out Quaternion rotation, out Vector3 scale)
+        {
+            TRS trs = ToTrs(mat);
+            position = trs.position;
+            rotation = trs.rotation;
+            scale = trs.scale;
+        }
+    }
+}
