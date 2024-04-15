790ed020ea240540ec0b0ba506f967c42ef3688c
ofsouzap <ofsouzap@gmail.com>
2024-04-15 17:19:42 +0100

    Dirtiness shader controller draw circle

--- Assets/Scripts/Minigames/Cleaning/DirtyBoneShaderController.cs
+++ Assets/Scripts/Minigames/Cleaning/DirtyBoneShaderController.cs
@@ -1,19 +1,30 @@
-﻿using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace BonesVr.Minigames.Cleaning
 {
     public class DirtyBoneShaderController : MonoBehaviour
     {
         [SerializeField] private int _dirtinessTextureSize = 512;
-        protected int DirtinessTextureSize => _dirtinessTextureSize;
+        public int DirtinessTextureSize => _dirtinessTextureSize;
+
+        private readonly int _dirtinessTextureSizeSqr;
+        public int DirtinessTextureSizeSqr => _dirtinessTextureSizeSqr;
 
         [SerializeField] private Renderer _renderer;
         protected Renderer Renderer => _renderer;
 
         private Texture2D m_DirtinessTexture;
 
-        protected static byte DirtinessValueToTexVal(float dirtinessValue) => (byte)Mathf.RoundToInt(255 * dirtinessValue);
+        public DirtyBoneShaderController() : base()
+        {
+            _dirtinessTextureSizeSqr = _dirtinessTextureSize * _dirtinessTextureSize;
+        }
+
+        protected static Color32 DirtinessValueToTexCol(float dirtinessValue)
+        {
+            byte v = (byte)Mathf.RoundToInt(255 * dirtinessValue);
+            return new(v, v, v, 255);
+        }
 
         protected virtual void Awake()
         {
@@ -23,7 +34,7 @@ namespace BonesVr.Minigames.Cleaning
                 Debug.LogError("No renderer component set or found");
 
             m_DirtinessTexture = new(DirtinessTextureSize, DirtinessTextureSize, TextureFormat.RGB24, false);
-            FillDirtinessTexture(1f);
+            DrawFill(1f);
         }
 
         protected virtual void Start()
@@ -31,15 +42,52 @@ namespace BonesVr.Minigames.Cleaning
             Renderer.material.SetTexture("_DirtinessTexture", m_DirtinessTexture);
         }
 
-        protected void FillDirtinessTexture(float dirtiness)
+        public void DrawCircle(float dirtiness, float centerU, float centerV, float radius, bool applyChanges = true)
         {
-            byte val = DirtinessValueToTexVal(dirtiness);
-            Color32 col = new(val, val, val, 255);
+            Color32 col = DirtinessValueToTexCol(dirtiness);
 
-            Color32[] arr = new Color32[m_DirtinessTexture.width * m_DirtinessTexture.height];
+            int centerX = Mathf.FloorToInt(DirtinessTextureSize * centerU);
+            int centerY = Mathf.FloorToInt(DirtinessTextureSize * centerV);
+
+            int radiusPix = Mathf.FloorToInt(DirtinessTextureSize * radius);
+            int radiusPixSqr = radiusPix * radiusPix;
+
+            for (int dy = -radiusPix; dy <= radiusPix; dy++)
+            {
+                int dySqr = dy * dy;
+                int xRange = Mathf.RoundToInt(Mathf.Sqrt(radiusPixSqr - dySqr));
+
+                Color32[] arr = new Color32[(2 * xRange) + 1];
+                for (int i = 0; i < arr.Length; i++) arr[i] = col;
+
+                m_DirtinessTexture.SetPixels32(
+                    centerX - xRange,
+                    centerY + dy,
+                    (2 * xRange) + 1,
+                    1,
+                    arr
+                );
+            }
+
+            if (applyChanges)
+                m_DirtinessTexture.Apply();
+        }
+
+        public void DrawFill(float dirtiness, bool applyChanges = true)
+        {
+            Color32 col = DirtinessValueToTexCol(dirtiness);
+
+            Color32[] arr = new Color32[DirtinessTextureSizeSqr];
             for (int i = 0; i < arr.Length; i++) arr[i] = col;
 
             m_DirtinessTexture.SetPixels32(arr);
+
+            if (applyChanges)
+                m_DirtinessTexture.Apply();
+        }
+
+        public void DrawApply()
+        {
             m_DirtinessTexture.Apply();
         }
     }
