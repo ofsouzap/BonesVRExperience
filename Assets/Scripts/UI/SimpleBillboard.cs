c6dd9cd3e6728ee3a81eafabd4b35f76b1baa8a9
ofsouzap <ofsouzap@gmail.com>
2024-04-17 21:22:34 +0100

    Skull and mandible EduBone labels

---
+++ Assets/Scripts/UI/SimpleBillboard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace BonesVr.Ui
+{
+    public class SimpleBillboard : MonoBehaviour
+    {
+        [SerializeField] private bool _oppositeDirection;
+        protected bool OppositeDirection => _oppositeDirection;
+
+        protected virtual void Update()
+        {
+            Transform camera = Camera.main.transform;
+            transform.rotation = Quaternion.LookRotation((OppositeDirection ? -1 : 1) * (camera.position - transform.position));
+        }
+    }
+}
