71186c660b6de7001c348ecde996aaa59dd2e662
ofsouzap <ofsouzap@gmail.com>
2024-04-19 15:15:21 +0100

    Add NPC text box

--- Assets/Scripts/Characters/Npcs/NpcTextBox.cs
+++ Assets/Scripts/Characters/Npcs/NpcTextBox.cs
@@ -3,12 +3,30 @@ using UnityEngine;
 
 namespace BonesVr.Characters.Npcs
 {
+    [RequireComponent(typeof(Canvas))]
     public class NpcTextBox : MonoBehaviour
     {
+        protected Canvas Canvas => GetComponent<Canvas>();
+
         [SerializeField] private TMP_Text _text;
         protected TMP_Text Text => _text;
 
+        protected virtual void Awake()
+        {
+            SetText(null);
+        }
+
         public void SetText(string text)
-            => Text.text = text;
+        {
+            Text.text = text;
+
+            if (string.IsNullOrEmpty(text))
+                HideTextBox();
+            else
+                ShowTextBox();
+        }
+
+        private void ShowTextBox() => Canvas.enabled = true;
+        private void HideTextBox() => Canvas.enabled = false;
     }
 }
