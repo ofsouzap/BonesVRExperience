8db2e345d8adda4bcb7e1393c05b8922f055ece2
ofsouzap <ofsouzap@gmail.com>
2024-04-20 15:26:46 +0100

    Fix issue teleporting player through collider

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,15 +1,37 @@
 ﻿using BonesVr.Utils;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace BonesVr.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        [Tooltip("The character controller for the player. If not set, will find it in the game object or its chlidren")]
+        [SerializeField] private CharacterController _characterController;
+        protected CharacterController CharacterController => _characterController;
+
+        protected virtual void Awake()
+        {
+            if (_characterController == null)
+                _characterController = GetComponentInChildren<CharacterController>();
+            if (_characterController == null)
+                Debug.LogError("No character controller set or found");
+        }
+
         public static PlayerController SceneInstance(GameObject gameObject) => SceneSingleton<PlayerController>.SceneInstance(gameObject.scene);
 
-        public void SetPlayerPosition(Vector3 pos) => transform.position = pos;
-        public void SetPlayerRotation(Quaternion rot) => transform.rotation = rot;
+        public void SetPlayerPosition(Vector3 pos)
+        {
+            // Need to temporarily disable character controller so it doesn't register a collision.
+            CharacterController.enabled = false;
+            transform.position = pos;
+            CharacterController.enabled = true;
+        }
+        public void SetPlayerRotation(Quaternion rot)
+        {
+            // Need to temporarily disable character controller so it doesn't register a collision.
+            CharacterController.enabled = false;
+            transform.rotation = rot;
+            CharacterController.enabled = true;
+        }
     }
 }
